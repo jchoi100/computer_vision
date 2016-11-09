a37945e7d3d0466a137b1039b9c8c13c0d8bb609
Joon Hyuck Choi <joonhyuck.choi92@gmail.com>
2016-11-09 16:38:10 -0500

    Compute depth image

--- HW3/p2_new.py
+++ HW3/p2_new.py
@@ -2,53 +2,67 @@ import cv2
 import numpy as np
 from math import sqrt
 
-El = cv2.imread('scene_l.bmp', 0)
-Er = cv2.imread('scene_r.bmp', 0)
+El, Er = cv2.imread('scene_l.bmp', 0), cv2.imread('scene_r.bmp', 0)
+b, f = 100.0, 400.0
 output_file = open('jchoi100_hw3_3d_point_cloud.txt', 'w')
+LEFT_MARGIN_BUFFER = 100
+WINDOW_SIZE = 15
 
 def compute_ncc(u, v, u2):
     denom1, denom2, numer = 0.0, 0.0, 0.0
-    for i in range(15):
-        for j in range(15):
+    for i in range(WINDOW_SIZE):
+        for j in range(WINDOW_SIZE):
             denom1 += El[v + j][u + i]**2
             denom2 += Er[v + j][u2 + i]**2
             numer += (El[v + j][u + i] - Er[v + j][u2 + i])**2
     return numer / sqrt(denom1 * denom2)
 
-b = 100.0
-f = 400.0
+depth_map = np.zeros(El.shape)
+num_rows, num_cols = len(El), len(El[0])
 
-num_rows = len(El)
-num_cols = len(El[0])
-
-three_D_points = []
-
-for v in range(num_rows - 15):
-    for u in range(num_cols - 15):
+for v in range(num_rows - WINDOW_SIZE):
+    for u in range(num_cols - WINDOW_SIZE):
         # v is fixed even in Er
-        max_ncc, max_u2 = -float('inf'), 0
-        if u > 100:
-            for u2 in range(u - 100, u):
+        max_ncc, max_u2 = -float('inf'), 0.0
+        if u > LEFT_MARGIN_BUFFER:
+            for u2 in range(u - LEFT_MARGIN_BUFFER, u):
                 ncc = compute_ncc(u, v, u2)
                 if ncc > max_ncc:
                     max_ncc, max_u2 = ncc, u2
         else:
+            # Ignore points that are in the left margins of the left image.
             max_u2 = -1
-            # for u2 in range(num_cols - 15):
-            #     ncc = compute_ncc(u, v, u2)
-            #     if ncc > max_ncc:
-            #         max_ncc, max_u2 = ncc, u2
         try:
             z = b * f / (u - max_u2)
             x = z * u / f
             y = z * v / f
-            output_file.write('(' + str(x) + ', ' + str(y) + ', ' + str(z) + ')\n')
-            three_D_points.append((x, y, z))
         except:
-            print("There was no disparity! Writing \'No Disparity.\' to output file...")
-            output_file.write('No Disparity\n')
+            print("There was no disparity! Writing (-1, -1, -1) to output file...")
+            x, y, z = -1, -1, -1
+            depth_map[v][u] = z
+        output_file.write('(' + str(x) + ', ' + str(y) + ', ' + str(z) + ')\n')
 
-# for (x, y, z) in three_D_points:
-    # print(x, y, z)
+for v in range(num_rows):
+    for u in range(num_cols):
+        if depth_map[v][u] == -1:
+            if v == 0 and u == 0:
+                depth_map[v][u] = (depth_map[v+1][u] + depth_map[v][u+1]) / 2
+            elif v == 0 and u == num_cols - 1:
+                depth_map[v][u] = (depth_map[v+1][u] + depth_map[v][u-1]) / 2
+            elif v == num_rows - 1 and u == 0:
+                depth_map[v][u] = (depth_map[v-1][u] + depth_map[v][u+1]) / 2
+            elif v == num_rows - 1 and u == num_cols - 1:
+                depth_map[v][u] = (depth_map[v-1][u] + depth_map[v][u-1]) / 2
+            elif v == 0:
+                depth_map[v][u] = (depth_map[v][u-1] + depth_map[v][u+1] + depth_map[v+1][u]) / 3
+            elif u == 0:
+                depth_map[v][u] = (depth_map[v-1][u] + depth_map[v+1][u] + depth_map[v][u+1]) / 3
+            elif v == num_rows - 1:
+                depth_map[v][u] = (depth_map[v][u-1] + depth_map[v][u+1] + depth_map[v-1][u]) / 3
+            elif u == num_cols - 1:
+                depth_map[v][u] = (depth_map[v+1][u] + depth_map[v-1][u] + depth_map[v][u-1]) / 3
+            else:
+                depth_map[v][u] = (depth_map[v-1][u] + depth_map[v+1][u] + depth_map[v][u-1] + depth_map[v][u+1]) / 4
 
-
+depth_map *= (255.0/depth_map.max())
+cv2.imwrite('jchoi100_hw3_depth_map.jpg', depth_map)
